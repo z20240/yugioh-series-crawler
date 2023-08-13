27d72c868ed06d788b71b2de194d0282a71cc902
les <130134817+leslee-glints@users.noreply.github.com>
2023-08-14 00:57:10 +0800

    feat: change code.

--- data.ts
+++ data.ts
@@ -7,12 +7,24 @@ export const data = {
   //   "range": [65131, 65616]
   // },
 
-  "20TH": {
-    "chTitle": "20週年紀念",
-    "series": "20TH",
+  // "20TH": {
+  //   "chTitle": "20週年紀念",
+  //   "series": "20TH",
+  //   "period": 11,
+  //   "range": [26026, 26067],
+  // },
+  "LGB1": {
+    "chTitle": "2019聖誕黃金禮盒",
+    "series": "LGB1",
     "period": 11,
-    "range": [26026, 26067],
-  }
+    "range": [37318, 37365],
+  },
+  "SSB1": {
+    "chTitle": "2021聖誕銀密閃盒",
+    "series": "SSB1",
+    "period": 11,
+    "range": [54003, 54075],
+  },
 
   // "1201": {
   //   "chTitle": "霸王世紀",
